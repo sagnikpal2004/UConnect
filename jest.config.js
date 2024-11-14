5927022e3d7cf5133c3afec203457c67cd5cedee
Hsiang-Chun Chiang <hsiangchunch@umass.edu>
2024-11-13 23:06:09 -0500

    Comments added

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   preset: 'ts-jest',  // Use ts-jest for transforming TypeScript files
-  testEnvironment: 'node',  // Use Node environment (can change to jsdom if working with frontend)
+  testEnvironment: 'node',  // Use Node environment
   globals: {
     'ts-jest': {
       tsconfig: 'testing/tsconfig.json',  // Path to the tsconfig.json for the tests
     },
   },
-  roots: ['<rootDir>/testing'],  // The folder where your test files are located
+  roots: ['<rootDir>/testing'],  // The folder where test files are located
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',  // Use ts-jest to transform .ts and .tsx files
   },
-  testMatch: ['**/testing/**/*.test.ts'],  // The pattern to match your test files
+  testMatch: ['**/testing/**/*.test.ts'],  // The pattern to match test files
 };
